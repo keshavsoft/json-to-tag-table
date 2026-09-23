9748a67082a6fd88f464f41f3fc603a2345f83e1
keshavsoft <nknnkumar@yahoo.com>
2026-09-23 15:09:00 +0530

    src 9

---
+++ src/v9/table/methods/render/index.js
@@ -0,0 +1,83 @@
+import skeletonToSpec from "../../../../skeletonToSpec/v3/index.js";
+
+// import jsonToSpec from "../../../../../node_modules/json-to-spec/index.js";
+// import jsonToTag from "../../../../../node_modules/@keshavsoft/json-to-tag/index.js";
+
+import jsonToSpec from "../../../../../jsonToSpec/v2/index.js";
+import jsonToTag from "../../../../../jsonToTag/v1/index.js";
+
+import skeletonJson from './skeleton.json' with {type: 'json'};
+import fragmentsJson from './fragments.json' with {type: 'json'};
+
+const removeSlot = ({ inNode, inSlotName } = {}) => {
+    const localNode = inNode;
+    const localSlotName = inSlotName;
+
+    if (!localNode || typeof localNode !== "object") return;
+
+    if (Array.isArray(localNode.slots)) {
+        localNode.slots = localNode.slots.filter(slot => slot !== localSlotName);
+    }
+
+    if (Array.isArray(localNode.children)) {
+        localNode.children.forEach(child => removeSlot({ inNode: child, inSlotName: localSlotName }));
+    }
+};
+
+const startFunc = ({
+    targetHtmlId,
+    inTargetHtmlId,
+    inColumns,
+    inData,
+    inColGroup,
+    inFooterData = [],
+    inConfig = {},
+    inSkeletonType = "default"
+} = {}) => {
+    const localTargetHtmlId = inTargetHtmlId ?? targetHtmlId;
+    const localColumns = inColumns;
+    const localData = inData;
+    const localColGroup = inColGroup;
+    const localFooterData = inFooterData;
+    const localConfig = inConfig;
+    const localSkeletonType = inSkeletonType;
+
+    try {
+        let dataAsJson = {};
+        dataAsJson.columns = localColumns;
+        dataAsJson.data = localData;
+        dataAsJson.colGroup = localColGroup;
+        dataAsJson.foot = localFooterData;
+        dataAsJson.title = localConfig?.title ?? localConfig?.caption?.text ?? "";
+        dataAsJson.footerText = localConfig?.footerText ?? "";
+
+        const rawSkeleton = skeletonJson[localSkeletonType] ?? skeletonJson.default ?? skeletonJson;
+        const targetSkeleton = structuredClone(rawSkeleton);
+
+        const structureJson = skeletonToSpec({
+            inSkeleton: targetSkeleton,
+            inFragments: fragmentsJson
+        });
+
+        const specAsJsonToDom = jsonToSpec({ specJson: structureJson, dataJson: dataAsJson, showLog: false });
+        console.log("specAsJsonToDom--- : ", specAsJsonToDom);
+
+        const fromRenderer = jsonToTag(specAsJsonToDom);
+
+        const html = document.getElementById(localTargetHtmlId);
+
+        html.addEventListener('click', function (event) {
+            const localButton = event.target.closest('button');
+            const localcurrentTarget = event.currentTarget;
+            console.log('Hooked element data:', localButton);
+        });
+
+        html.append(fromRenderer);
+    } catch (error) {
+        console.log("error : ", error);
+    };
+};
+
+export default startFunc;
+
+
---
+++ src/v9/table/tableStore/buildLibrary/rows/index.js
@@ -0,0 +1,30 @@
+import { cloneData } from "../../../../common/cloneData.js";
+import insertSerial from "./serial/index.js";
+
+const buildRows = ({ inData = [], inLayout = {}, inConfig = {} } = {}) => {
+    const localData = inData;
+    const localLayout = inLayout;
+    const localConfig = inConfig;
+
+    const stateData = cloneData({
+        inData: localData,
+        inActiveColumns: localLayout.activeColumns
+    });
+
+    const serialResult = insertSerial({
+        inColumns: localLayout.activeColumns,
+        inData: stateData,
+        inConfig: localConfig,
+        inColGroup: localLayout.colGroup
+    });
+
+    return {
+        columns: serialResult.columns,
+        stateData: serialResult.data,
+        isSerialEnabled: serialResult.isSerialEnabled,
+        colGroup: serialResult.colGroup
+    };
+};
+
+export { buildRows };
+export default buildRows;
